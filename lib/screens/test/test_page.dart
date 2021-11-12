52ce7b981aa337e54a0c245c687853cb45b49e17
Tsiuryn_AV <tsiuryn_av@st.by>
2021-11-12 16:14:58 +0300

    test dialog

--- lib/screens/test/test_page.dart
+++ lib/screens/test/test_page.dart
@@ -18,6 +18,7 @@ class _TestPageState extends State<TestPage> {
   late Timer _timer;
   @override
   Widget build(BuildContext context) {
+    double c_width = MediaQuery.of(context).size.width*0.8;
     return Scaffold(
         appBar: AppBar(
           title: Text('Test page'),
@@ -35,7 +36,76 @@ class _TestPageState extends State<TestPage> {
                   //   });
                   // });
               }),
+
+          MaterialButton(
+              child: Text("show dialog"),
+              color: Colors.blue,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4.0)),
+              onPressed: () {
+                _showLogoutDialog(c_width);
+              }),
         ]));
+  }
+
+  void _showLogoutDialog(double width) {
+    // if(event != null){
+    showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => SimpleDialog(
+          children: <Widget>[
+
+
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Stack(
+                alignment: AlignmentDirectional.topEnd,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 24.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Center(
+                          child: Padding(
+                            padding: const EdgeInsets.only(right: 16.0),
+                            child: Icon(Icons.download, size: 50,),
+                          ),
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'Attention!',
+                              ),
+                              Text(
+                                'Hello hello Hello hello Hello hello Hello hello Hello hello Hello hello Hello hello ',
+                              ),
+                            ],
+                          ),
+                        ),
+
+
+
+
+
+                      ],
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Icon(Icons.close),
+                  ),
+
+                ],
+              ),
+            ),
+
+          ],
+        ));
+    // }
   }
 
   void _startTimer(Function callback){
