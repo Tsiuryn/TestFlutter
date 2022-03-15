10225109622d60a3ca7db1a6bc65b4a5405232e5
Alex Tsiuryn <>
2022-03-15 19:53:03 +0300

    added test Uuid and lists

---
+++ lib/dart/collection/test_1.dart
@@ -0,0 +1,23 @@
+final List<int> example = [1,2,3,4,5,6];
+
+const myId = 5;
+
+void main(){
+  var state = MyState(example);
+
+  for (var i = 0; i < state.person.length; ++i) {
+    var o = state.person[i];
+    if(o == myId){
+      state.person[i] = 77;
+    }
+
+  }
+
+  print(state.person);
+}
+
+class MyState{
+  final List<int> person;
+
+  MyState(this.person);
+}
---
+++ lib/dart/collection/test_2_uuid.dart
@@ -0,0 +1,13 @@
+import 'package:uuid_type/uuid_type.dart';
+
+final List<int> example = [1,2,3,4,5,6];
+
+const myId = 'ec949438-a1c0-4555-aacb-284a1e0c4df0';
+const myId1 = 'ec949438-a1c0-4555-aacb-284a1e0c4df1';  // // TODO: last symbol changed
+
+void main(){
+  var myUuid = Uuid.tryParse(myId);
+  var myUuid1 = Uuid.tryParse(myId1);
+
+  print(myUuid == myUuid1);
+}
