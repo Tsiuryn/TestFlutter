3f0a7be8cda6467ef62ab920063beb16890c8d36
Alex Tsiuryn <>
2022-04-10 19:24:05 +0300

    added auto generator class

---
+++ lib/screens/file_generator/my_file_generator.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+part 'my_file_generator.g.dart';
+
+class MyFileGenerator extends $AppWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return super.build(context);
+  }
+}
---
+++ lib/screens/file_generator/my_file_generator.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'my_file_generator.dart';
+
+// **************************************************************************
+// WidgetGenerator
+// **************************************************************************
+
+class $AppWidget extends StatelessWidget {
+  const $AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Oll right'),
+    );
+  }
+}
